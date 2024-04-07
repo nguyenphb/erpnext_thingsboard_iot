831b9fc79a0b2c61de3c766389cbed51eb48e586
baonguyen2409 <baonguyen2409@gmail.com>
2024-04-07 22:08:14 +0700

    update

---
+++ erpnext_thingsboard_iot/erpnext_thingsboard_iot/doctype/control_device_history/control_device_history.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, VIIS IoT Solution and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('control_device_history', {
+	// refresh: function(frm) {
+
+	// }
+});
